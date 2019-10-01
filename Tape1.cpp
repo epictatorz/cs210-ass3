cb2ab308f65b03cc851c79dead81b5cb94a8032d
Palliser7 <56008018+Palliser7@users.noreply.github.com>
2019-10-01 00:20:54 -0600

    Create Tape1.cpp
    see if this compiles this is what I have so far.

---
+++ Tape1.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include "ActionTable.h"
+#include "Tape.h"
+using namespace std;
+
+//const int AT_Size = 25;
+//void turing(int, ActionTable, tape);
+
+int main() {
+	bool exit = false;
+	char choice;
+	while (exit == false) {							//simple menu system
+		cout << "Please enter a choice." << endl;
+		cout << "1: Count File" << endl;
+		cout << "2: Subtract File" << endl;
+		cout << "3: Exit" << endl;
+		cin >> choice;
+		switch (choice) {
+		case '1': {
+			int StateRegister;
+			tape T;
+			ActionTable AT[AT_Size];
+			string Text = "problem.txt";	//sets file name
+			string Line;
+
+
+			ifstream problem;				//opens the file
+			problem.open(Text);
+
+			getline(problem, Line);
+			StateRegister = Line[0]- '0';		//initilizes the StateRegister
+
+			getline(problem, Line);
+			unsigned int count = 2;
+
+			T.iHead(Line[0]);
+			while (count <= Line.length() + 1) {
+				T.iTail(Line[count]);
+				count += 2;
+			}
+
+			char Symbol = '0';
+			while (Symbol != '-') {			//this loop initilizes the Action Table
+				int i = 0;
+				getline(problem, Line);
+				//cout << Line << endl;
+				Symbol = Line[0];			//gets the first character in the list and sees if it is '-' to end the 
+				if (Symbol == '-') {		//initilizing of the Action Table
+					break;
+				}
+				AT[i].CurrentState = int(Line[0]) - 48;		//must subtract 48 to convert the ASCII code to integers
+				AT[i].Symbol = Line[2];				//skips odds as they are spaces ' '
+				AT[i].NextState = int(Line[4]) - 48;
+				AT[i].NewSymbol = Line[6];
+				AT[i].Direction = Line[8];
+				//cout << AT[i].CurrentState << AT[i].Symbol << AT[i].NextState << AT[i].NewSymbol << AT[i].Direction << endl;
+				i++;
+			}
+			getline(problem, Line);
+			//cout << Line << endl;
+			for (int i = 0; i <= (Line[0] - '0') - 1; i++){
+				T.itR();
+				cout << T.read() << "  ";
+			}
+			cout << endl;
+			problem.close();
+			int x;
+			for (int j = 0; j < 5; j++) {
+				cout << T.read() << endl;
+				//cout << "SR:" << StateRegister << "  Value:" << T.read() << "   Pointer Position:" << T.getP() << endl;
+				switch (T.read()) {
+				case '0':
+					switch (StateRegister) {
+					case 1:
+						T.write(1);
+						cout << endl << T.read() << endl;
+						T.pList();
+						x = 1;
+						StateRegister = 0;
+						break;
+					}
+					break;
+				case '1':
+					switch (StateRegister) {
+					case 0:
+						T.itR();
+						T.pList();
+						break;
+					case 1:
+						T.write(0);
+						T.itL();
+						T.pList();
+						break;
+					}
+					break;
+				case 'B':
+					switch (StateRegister) {
+					case 0:
+						cout << "hi  " << T.read() << endl;
+						//T.itL();
+						T.pList();
+						T.itL();
+						cout << T.read() << endl;
+						StateRegister = 1;
+						cout << StateRegister << endl;
+						break;
+					case 1:
+						int x = 1;
+					}
+					break;
+				}
+			}
+
+
+
+			//turing(StateRegister, AT[AT_Size], T);
+			break;
+		}
+		case '2':
+			void subtract();
+			break;
+		case '3':
+			exit = true;
+			break;
+		default:
+			cout << "Invalid choice." << endl;
+		}
+	}
+	return 0;
+}
+//void turing(int SR, ActionTable AT, tape T) {
+	/*for (int i = 0; i <= AT_Size; i++) {
+		switch()
+	}*/
+//}
+/*
+void subtract() {
+	int StateRegister;
+	ActionTable AT[AT_Size];
+	string Text = "problem.txt";
+	string Line;
+
+	ifstream problem;
+	problem.open(Text);
+	getline(problem, Line);
+	StateRegister = Line[1];		//initilizes the StateRegister
+
+	char Symbol = '0';
+	while (Symbol != '-') {			//this loop initilizes the Action Table
+		int i = 0;
+		getline(problem, Line);
+		//test line	cout << Line << endl;
+		Symbol = Line[0];			//gets the first character in the list and sees if it is '-' to end the 
+		if (Symbol == '-') {		//initilizing of the Action Table
+			break;
+		}
+		AT[i].CurrentState = int(Line[0]) - 48;		//must subtract 48 to convert the ASCII code to integers
+		AT[i].Symbol = int(Line[2]);				//skips odds as they are spaces ' '
+		AT[i].NextState = Line[4] - 48;
+		AT[i].NewSymbol = Line[6];
+		AT[i].Direction = Line[8];
+		//test line cout << AT1[i].CurrentState << AT1[i].Symbol << AT1[i].NextState << AT1[i].NewSymbol << AT1[i].Direction << endl;
+		i++;
+	}
+
+	problem.close();
+}
+*/
